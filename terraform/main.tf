1fa5394658f4f1c786b62b70daeade9eef94bfee
Andrei Maiboroda <am@homembr.ru>
2019-12-29 20:15:42 +0300

    before star task

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,42 +7,62 @@ provider "google" {
   # Версия провайдера
   version = "2.15"
   # ID проекта
-  project = "infra-262320"
-  region = "europe-west-1"
+  #project = "infra-262320"
+  #region = "europe-west-1"
+  project = var.project
+  region  = var.region
 }
 
 resource "google_compute_instance" "app" {
-  name = "reddit-app"
+  name         = "reddit-app"
   machine_type = "g1-small"
-  zone = "europe-west1-b"
-  tags = ["reddit-app"]
+  zone         = var.zone
+  tags         = ["reddit-app"]
   boot_disk {
     initialize_params {
-      image = "reddit-base"
+      image = var.disk_image
     }
-   }
- metadata = {
-   # путь до публичного ключа
-   ssh-keys = "appuser:${file("~/.ssh/appuser.pub")}"
-   }
-
- network_interface {
-   network = "default"
-   access_config {}
-   }
+  }
+  metadata = {
+    # путь до публичного ключа
+    ssh-keys = "appuser:${file(var.public_key_path)}"
   }
 
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    host        = self.network_interface[0].access_config[0].nat_ip
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+
+}
+
 resource "google_compute_firewall" "firewall_puma" {
- name = "allow-puma-default"
- # Название сети, в которой действует правило
- network = "default"
- # Какой доступ разрешить
- allow {
-   protocol = "tcp"
-   ports = ["9292"]
- }
- # Каким адресам разрешаем доступ
- source_ranges = ["0.0.0.0/0"]
- # Правило применимо для инстансов с перечисленными тэгами
- target_tags = ["reddit-app"]
+  name = "allow-puma-default"
+  # Название сети, в которой действует правило
+  network = "default"
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable project {
+  description = "Project ID"
+}
+variable region {
+  description = "Region"
+  # Значение по умолчанию
+  default = "europe-west1"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "Path to the public key used for ssh access"
+}
+variable disk_image {
+  description = "Disk image"
+}
+variable private_key_path {
+  description = "path to private key"
+}
+variable zone {
+  description = "zone for instance"
+  default     = "europe-west1-b"
+}
