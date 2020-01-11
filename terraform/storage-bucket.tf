8dec6ed9a0b76c7a44f342a1bdcb2ed034af1648
Andrei Maiboroda <am@homembr.ru>
2020-01-11 23:42:11 +0300

    bucket.tf fix

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,17 +1,17 @@
-#provider "google" {
+provider "google" {
 #  version = "~> 2.5"
 ##  #version = "2.0.0"
-#  project = "${var.project}"
-#  region  = "${var.region}"
-#}
+  project = "${var.project}"
+  region  = "${var.region}"
+}
 
-#module "storage-bucket" {
-#  source   = "SweetOps/storage-bucket/google"
-#  version  = "0.3.0"
-#  name     = "mbrbug-bucket-reddit-app"
-#  location = "europe-west1"
-#}
+module "storage-bucket" {
+  source   = "SweetOps/storage-bucket/google"
+  version  = "0.3.0"
+  name     = "mbrbug-bucket-reddit-app"
+  location = "europe-west1"
+}
 
-#output storage-bucket_url {
-#  value = "${module.storage-bucket.url}"
-#}
+output storage-bucket_url {
+  value = "${module.storage-bucket.url}"
+}
